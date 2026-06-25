6f9d48a0d26e2e2609df71f9d9b107a6db1cb6a1
Nicolas <adilsonq1976@gmail.com>
2026-06-25 13:14:12 -0300

    Criando Classe NotificacaoEmail

--- src/NotificacaoEmail.java
+++ src/NotificacaoEmail.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+public class NotificacaoEmail implements Notificavel{
+    private String enderecoEmail;
+    private String nomeRemetente;
 
-public class NotificacaoEmail {
+    public NotificacaoEmail(String enderecoEmail, String nomeRemetente) {
+        this.enderecoEmail = enderecoEmail;
+        this.nomeRemetente = nomeRemetente;
+    }
+
+    @Override
+    public void enviarNotificacao(String mensagem){
+        System.out.println("\uD83D\uDCE7 [EMAIL}");
+        System.out.println("De: " + nomeRemetente);
+        System.out.println("Para: " + enderecoEmail);
+        System.out.println("Mensagem: " + mensagem);
+        System.out.println();
+    }
+
+    @Override
+    public String getTipoCanal(){
+        return "E-mail";
+    }
+
 }
