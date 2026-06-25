41661f5477a7c6e62454510b353212baf411937f
Nicolas <adilsonq1976@gmail.com>
2026-06-25 13:26:31 -0300

    Criando Classe ServicoDeNotificacao

--- src/ServicoDeNotificacao.java
+++ src/ServicoDeNotificacao.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServicoDeNotificacao {
+    private List<Notificavel> canais = new ArrayList<>();
+
+    public void adicionarCanal(Notificavel canal){
+        canais.add(canal);
+    }
+
+    public void notificarTodos(String mensagem){
+        System.out.println("=== Deisparando notificacoes ===");
+        for(Notificavel canal : canais){
+            canal.enviarNotificacao(mensagem);
+        }
+    }
+
+    public void notificarPorTipo(String tipo, String mensagem){
+        for(Notificavel canal : canais){
+            if(canal.getTipoCanal().equalsIgnoreCase(tipo)){
+                canal.enviarNotificacao(mensagem);
+            }
+        }
+    }
+
+    public void listarCanais(){
+        System.out.println("Canais cadastrados:");
+        for(Notificavel canal : canais){
+            System.out.println("- " + canal.getTipoCanal());
+        }
+    }
 }
