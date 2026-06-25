32e9d55cd931d5d1dc10a537faa1bbcf6893c8da
Nicolas <adilsonq1976@gmail.com>
2026-06-25 13:19:36 -0300

    Criando Classe NotificacaoPush

--- src/NotificacaoPush.java
+++ src/NotificacaoPush.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+public class NotificacaoPush implements Notificavel{
+    private  String dispositivoId;
+    private String appNome;
 
-public class NotificacaoPush {
+    public NotificacaoPush(String appNome, String dispositivoId) {
+        this.appNome = appNome;
+        this.dispositivoId = dispositivoId;
+    }
+
+    @Override
+    public void enviarNotificacao(String mensagem) {
+        System.out.println("🔔 [PUSH]");
+        System.out.println("App: " + appNome);
+        System.out.println("Dispositivo: " + dispositivoId);
+        System.out.println("Mensagem: " + mensagem);
+        System.out.println();
+    }
+
+    @Override
+    public String getTipoCanal() {
+        return "Push";
+    }
 }
