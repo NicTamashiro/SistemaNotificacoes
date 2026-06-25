c8976f311e7b4d36517474bbd99af2a38899e9ca
Nicolas <adilsonq1976@gmail.com>
2026-06-25 13:16:42 -0300

    Criando Classe NotificacaoSMS

--- src/NotificacaoSMS.java
+++ src/NotificacaoSMS.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+public class NotificacaoSMS implements Notificavel{
+    private String numeroTelefone;
 
-public class NotificacaoSMS {
+    public NotificacaoSMS(String numeroTelefone) {
+        this.numeroTelefone = numeroTelefone;
+    }
+
+    @Override
+    public void enviarNotificacao(String mensagem) {
+        if (mensagem.length() > 160){
+            System.out.println("⚠ SMS muito longo, será cortado.");
+            mensagem = mensagem.substring(0, 160);
+        }
+        System.out.println("📱 [SMS]");
+        System.out.println("Para: " + numeroTelefone);
+        System.out.println("Mensagem: " + mensagem);
+        System.out.println();
+    }
+
+    @Override
+    public String getTipoCanal() {
+        return "SMS";
+    }
 }
